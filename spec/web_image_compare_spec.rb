e240cd0bbd805040435ed66b09974112c59c389a
Rui Nunes <rui@brandwatch.com>
2016-02-03 09:46:26 +0000

    Updating tests - Checking process_image method

--- spec/web_image_compare_spec.rb
+++ spec/web_image_compare_spec.rb
@@ -7,4 +7,12 @@ describe WebImageCompare do
       expect(WebImageCompare.new(image_path, image_path)).to be_a WebImageCompare
     end
   end
+
+  describe '.process_image' do
+    it 'is a perfect match' do
+      image_path = File.dirname(__FILE__) +  '/images/example_image.png'
+      compare_image = WebImageCompare.new(image_path, image_path)
+      compare_image.process_image(false, {}).should eq(0.0)
+    end
+  end
 end
