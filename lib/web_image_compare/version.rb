45a4937d170ac19110f5069420a947ae0f196ea5
Rui Nunes <rui@brandwatch.com>
2016-07-06 10:27:02 +0100

    Fixing the include of the color module

--- lib/web_image_compare/version.rb
+++ lib/web_image_compare/version.rb
@@ -2,9 +2,9 @@ module CmpImgTool
   # BW config api Version
   module Version
     # The current gem version
-    VERSION = '0.0.2'
+    VERSION = '0.0.3'
     # The date version was updated
-    DATE    = '2016-02-03'
+    DATE    = '2016-07-06'
     # Debug output that will contain ruby information
     MSG     = 'Version %s %s (running on %s-%s)'
 
